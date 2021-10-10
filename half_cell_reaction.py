e77cb5e67adf0298399ad7216dd68f4a6f50cf35
Sumit-Dey-2002 <sumitdeypapan2002@gmail.com>
2021-10-10 11:06:59 +0530

    git init

---
+++ half_cell_reaction.py
@@ -0,0 +1,88 @@
+# """ import numpy as np
+
+def balance_acid(reagent,product):
+    # Balancing Central Atom START
+    lcm = np.lcm(reagent['central_atom'], product['central_atom'])
+    multiplier = [lcm / reagent['central_atom'], lcm / product['central_atom']]
+
+    for i in reagent:
+        reagent[i]=reagent[i]*multiplier[0]
+        product[i]=product[i]*multiplier[1]
+
+    # Balancing Central Atom END
+    # print(reagent,product)
+
+    #Balancing Oxygen Atom Start
+    water_added= reagent['oxygen'] - product['oxygen']
+    # print(water_added)
+    oxygen_added=abs(water_added)
+    hydrogen_added=abs(water_added)*2
+    if water_added<0:
+        reagent['oxygen']+=oxygen_added
+        reagent['hydrogen']+=hydrogen_added
+    else:
+        product['oxygen']+=oxygen_added
+        product['hydrogen']+=hydrogen_added
+
+    #Balancing Oxygen Atom End
+    # print(reagent,product)
+
+    #Balancing Hydrogen Atom Start
+    hydrogen_ion_added= reagent['hydrogen']-product['hydrogen']
+    # print(hydrogen_ion_added)
+    hydrogen_added=abs(hydrogen_ion_added)
+    charge_added=abs(hydrogen_ion_added)
+    if hydrogen_ion_added<0:
+        reagent['hydrogen']+=hydrogen_added
+        reagent['charge']+=charge_added
+    else:
+        product['hydrogen']+=hydrogen_added
+        product['charge']+=charge_added
+
+    #Balancing Oxygen Atom End
+    # print(reagent,product)
+    return (reagent,product)
+
+def balance_base(reagent,product):
+#      MnO4¯ + S2¯ ---> MnS + S
+
+# 8H2O + 2MnO4¯ + 7S2¯ ---> 2MnS + 5S + 16OH¯
+    # Balancing Central Atom START
+    lcm = np.lcm(reagent['central_atom'], product['central_atom'])
+    multiplier = [lcm / reagent['central_atom'], lcm / product['central_atom']]
+
+    for i in reagent:
+        reagent[i]=reagent[i]*multiplier[0]
+        product[i]=product[i]*multiplier[1]
+
+    # Balancing Central Atom END
+    # print(reagent,product)
+
+    #Balancing Oxygen Atom Start
+    water_added= reagent['oxygen'] - product['oxygen']
+    # print(water_added)
+    oxygen_added=abs(water_added)
+    hydrogen_added=abs(water_added)*2
+    if water_added<0:
+        reagent['oxygen']+=oxygen_added
+        reagent['hydrogen']+=hydrogen_added
+    else:
+        product['oxygen']+=oxygen_added
+        product['hydrogen']+=hydrogen_added
+
+    #Balancing Oxygen Atom End
+    # print(reagent,product)
+
+    #Balancing Hydrogen Atom Start
+    hydrogen_ion_added= reagent['hydrogen']-product['hydrogen']
+    # print(hydrogen_ion_added)
+    hydrogen_added=abs(hydrogen_ion_added)
+    charge_added=abs(hydrogen_ion_added)
+    if hydrogen_ion_added<0:
+        reagent['hydrogen']+=hydrogen_added
+        reagent['charge']+=charge_added
+    else:
+        product['hydrogen']+=hydrogen_added
+        product['charge']+=charge_added
+
+    #Balancing Oxygen Atom End
---
+++ main.py
@@ -0,0 +1,39 @@
+import numpy as np
+import half_cell_reaction as hcr
+
+# reduction_rxn = {'reagent':{'central_atom':1,'oxygen':4,'hydrogen':0,'charge':-1}, 'product':{'central_atom':1,'oxygen':0,'hydrogen':0,'charge':2}}
+# oxidation_rxn = {'reagent':{'central_atom':2,'oxygen':4,'hydrogen':0,'charge':-2}, 'product':{'central_atom':1,'oxygen':2,'hydrogen':0,'charge':0}}
+
+# reduction_rxn = {'reagent':{'central_atom':1,'oxygen':4,'hydrogen':0,'charge':-1}, 'product':{'central_atom':1,'oxygen':0,'hydrogen':0,'charge':2}}
+# oxidation_rxn = {'reagent':{'central_atom':1,'oxygen':0,'hydrogen':0,'charge':-1}, 'product':{'central_atom':2,'oxygen':0,'hydrogen':0,'charge':0}}
+
+# reduction_rxn = {'reagent':{'central_atom':1,'oxygen':0,'hydrogen':0,'charge':+2}, 'product':{'central_atom':1,'oxygen':0,'hydrogen':0,'charge':0}}
+# oxidation_rxn = {'reagent':{'central_atom':1,'oxygen':0,'hydrogen':0,'charge':0}, 'product':{'central_atom':1,'oxygen':0,'hydrogen':0,'charge':+2}}
+
+reduction_rxn = {'reagent':{'central_atom':1,'oxygen':3,'hydrogen':0,'charge':-1}, 'product':{'central_atom' :1,'oxygen':0,'hydrogen':0,'charge':-1}}
+oxidation_rxn = {'reagent':{'central_atom':1,'oxygen':2,'hydrogen':0,'charge':0}, 'product':{'central_atom':1,'oxygen':4,'hydrogen':0,'charge':-2}}
+
+
+
+(reduction_rxn['reagent'],reduction_rxn['product']) = hcr.balance_acid(reduction_rxn['reagent'],reduction_rxn['product'])
+(oxidation_rxn['reagent'],oxidation_rxn['product']) = hcr.balance_acid(oxidation_rxn['reagent'],oxidation_rxn['product'])
+
+reduction_transferred_electron = int(reduction_rxn['reagent']['charge'] - reduction_rxn['product']['charge'])
+oxidation_transferred_electron = int(oxidation_rxn['product']['charge'] - oxidation_rxn['reagent']['charge'])
+
+# print(reduction_transferred_electron)
+# print(oxidation_transferred_electron)
+
+total_transferred_electron = np.lcm(reduction_transferred_electron,oxidation_transferred_electron)
+
+for i in reduction_rxn['reagent']:
+    reduction_multiplier = total_transferred_electron / reduction_transferred_electron
+    reduction_rxn['reagent'][i] *= reduction_multiplier
+    reduction_rxn['product'][i] *= reduction_multiplier
+
+    oxidation_multiplier = total_transferred_electron / oxidation_transferred_electron
+    oxidation_rxn['reagent'][i] *= oxidation_multiplier
+    oxidation_rxn['product'][i] *= oxidation_multiplier
+
+print(reduction_rxn)
+print(oxidation_rxn)
