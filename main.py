938bbe2b37ab46b6b22232246a567c9daeedebc9
Shirsendu Bairagi <shirsendu2001@gmail.com>
2021-11-07 13:37:14 +0530

    redox_identify renamed split_rxn, merge_rxn added

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
-from numpy import right_shift
-from molecule_identifier import Molecule, merge_molecule, redox_identify, print_rxn
+from molecule_identifier import Molecule, merge_rxn, split_rxn, print_rxn
 from half_cell_reaction import balance_acid, balance_base, balance_electron
-# ClO3¯ + SO2 ---> SO42¯ + Cl¯
-reaction = {"reactant": [Molecule('Cl1O3', 'Cl', -1),Molecule('S1O2', 'S', 0)],
-            "product": [Molecule('S1O4', 'S', -2), Molecule('Cl1', 'Cl', -1)]}
 
-print_rxn(reaction, "\n")
+
+# ClO3¯ + SO2 ---> SO42¯ + Cl¯
+# reaction = {'reactant': [Molecule('Cl1O3', 'Cl', -1),Molecule('S1O2', 'S', 0)],
+#             'product': [Molecule('S1O4', 'S', -2), Molecule('Cl1', 'Cl', -1)]}
 
 # Special Case: P4 ---> H2PO2 + PH3
-# reaction = {"reactant": [Molecule('P4', 'P', 0)],
-#             "product": [Molecule('P1H3', 'P', 0),Molecule('H2P1O2', 'P', -1)]}
+# reaction = {'reactant': [Molecule('P4', 'P', 0)],
+#             'product': [Molecule('P1H3', 'P', 0),Molecule('H2P1O2', 'P', -1)]}
 
-#Cl2 ---> ClO- + Cl-
-# red_rxn = {'reactant': [Molecule('Cl2', 'Cl', 0)], 'product': [Molecule('Cl1', 'Cl', -1)]}
-# ox_rxn = {'reactant': [Molecule('Cl2', 'Cl', 0)], 'product': [Molecule('Cl1O1', 'Cl', -1)]}
-
+# Cl2 ---> ClO- + Cl-
+# reaction = {'reactant': [Molecule('Cl2', 'Cl', 0), Molecule('Cl2', 'Cl', 0)],
+#             'product': [Molecule('Cl1', 'Cl', -1), Molecule('Cl1O1', 'Cl', -1)]}
 
 # H2O2 ---> H2O + O2
-# red_rxn = {'reactant': [Molecule('H2O2', 'O', 0)], 'product': [Molecule('H2O1', 'O', 0)]}
-# ox_rxn = {'reactant': [Molecule('H2O2', 'O', 0)], 'product': [Molecule('O2', 'O', 0)]}
+# reaction = {'reactant': [Molecule('H2O2', 'O', 0), Molecule('H2O2', 'O', 0)],
+#             'product': [Molecule('H2O1', 'O', 0), Molecule('O2', 'O', 0)]}
 
 # NH3 + ClO¯ ---> N2H4 + Cl¯
-# red_rxn = {'reactant': [Molecule('Cl1O1', 'Cl', -1)], 'product': [Molecule('Cl1', 'Cl', -1)]}
-# ox_rxn = {'reactant': [Molecule('N1H3', 'N', 0)], 'product': [Molecule('N2H4', 'N', 0)]}
+reaction = {'reactant': [Molecule('Cl1O1', 'Cl', -1), Molecule('N1H3', 'N', 0)],
+            'product': [Molecule('Cl1', 'Cl', -1), Molecule('N2H4', 'N', 0)]}
 
-(red_rxn,ox_rxn) = redox_identify(reaction)
+print_rxn(reaction, '\n')
+
+(red_rxn, ox_rxn) = split_rxn(reaction)
 
 print_rxn(red_rxn)
 print_rxn(ox_rxn, '\n')
@@ -32,7 +32,7 @@ print_rxn(ox_rxn, '\n')
 red_rxn = balance_base(red_rxn)
 ox_rxn = balance_base(ox_rxn)
 
-""" 
+''' 
 # NH3 + ClO¯ ---> N2H4 + Cl¯
 red_rxn = {'reactant': [Molecule('Cl1O1', 'Cl', -1)], 'product': [Molecule('Cl1', 'Cl', -1)]}
 ox_rxn = {'reactant': [Molecule('N1H3', 'N', 0)], 'product': [Molecule('N2H4', 'N', 0)]}
@@ -42,7 +42,7 @@ print_rxn(ox_rxn, '\n')
 
 red_rxn = balance_base(red_rxn)
 ox_rxn = balance_base(ox_rxn) 
-"""
+'''
 
 print_rxn(red_rxn)
 print_rxn(ox_rxn, '\n')
@@ -52,5 +52,6 @@ print_rxn(ox_rxn, '\n')
 print_rxn(red_rxn)
 print_rxn(ox_rxn, '\n')
 
-# TODO: Cancellation
-# merge_molecule()
+reaction = merge_rxn(red_rxn, ox_rxn)
+
+print_rxn(reaction)
--- molecule_identifier.py
+++ molecule_identifier.py
@@ -1,8 +1,6 @@
 import re
 
 
-
-
 class Molecule:
     def __init__(self, molecule, central_atom, charge, coff=1):
         atoms = {}
@@ -31,7 +29,7 @@ class Molecule:
         return self.charge * self.coff
 
     def get_os(self):
-        return (self.get_atom("H")*(-1)+self.get_atom("O")*(+2))/self.get_central_atom()
+        return (self.get_atom('H')*(-1)+self.get_atom('O')*(+2))/self.get_central_atom()
 
     # UPDATE: From ionic to covalent
     def set_atom(self, atom, num):
@@ -39,10 +37,8 @@ class Molecule:
             del self.atoms[atom]
         else:
             self.atoms[atom] = num
-    
 
-""" class Reaction:
-    def __init__(self): """
+
 def merge_molecule(reaction):
     # merge molecule of same side
     for side in reaction:
@@ -53,24 +49,50 @@ def merge_molecule(reaction):
                     # side[j].coff += side[i].coff
                     # del side[i]
 
-def redox_identify(reaction):
-    
-    red_rxn = {"reactant" : [],"product" : []}
-    ox_rxn = {"reactant" : [],"product" : []}
-    
+
+def split_rxn(reaction):
+    red_rxn = {'reactant': [], 'product': []}
+    ox_rxn = {'reactant': [], 'product': []}
+
     for reactant in reaction['reactant']:
         for product in reaction['product']:
-            
             if reactant.central_atom == product.central_atom:
-                if reactant.get_os()>product.get_os():
+                if reactant.get_os() > product.get_os():
                     red_rxn['reactant'].append(reactant)
                     red_rxn['product'].append(product)
-                else :
+                else:
                     ox_rxn['reactant'].append(reactant)
                     ox_rxn['product'].append(product)
-    return (red_rxn,ox_rxn)
 
-# def merge_rxn(reaction):
+    return (red_rxn, ox_rxn)
+
+
+def merge_rxn(red_rxn, ox_rxn):
+    reaction = {'reactant': [*red_rxn['reactant'], *ox_rxn['reactant']],
+                'product': [*red_rxn['product'], *ox_rxn['product']]}
+
+    print_rxn(reaction)
+
+    for r in range(len(reaction['reactant'])):
+        for p in range(len(reaction['product'])):
+            if reaction['reactant'][r].molecule == reaction['product'][p].molecule:
+                if reaction['reactant'][r].coff == 0 | reaction['product'][p].coff == 0:
+                    continue
+
+                if reaction['reactant'][r].coff > reaction['product'][p].coff:
+                    reaction['reactant'][r].coff -= reaction['product'][p].coff
+                    reaction['product'][p].coff = 0
+                elif reaction['reactant'][r].coff < reaction['product'][p].coff:
+                    reaction['product'][p].coff -= reaction['reactant'][r].coff
+                    reaction['reactant'][r].coff = 0
+                else:
+                    reaction['product'][p].coff = 0
+                    reaction['reactant'][r].coff = 0
+
+    reaction['reactant'] = filter(lambda molecule: bool(molecule.coff), reaction['reactant'])
+    reaction['product'] = filter(lambda molecule: bool(molecule.coff), reaction['product'])
+
+    return reaction
 
 
 def print_rxn(reaction, extra=''):
